dd55d8ce3c71f11acea2f9abe8f9d8621c65485e
hasnianilahi <iammrkemzy47@gmail.com>
2025-04-19 17:50:43 +0530

    Delete src/app/profile directory

--- src/app/profile/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function UserProfile({params}:any) {
-    return (
-        <div>
-            <h1>Profile</h1>
-            <p className="text-4xl">{params.id}</p>
-        </div>
-    )
-}
